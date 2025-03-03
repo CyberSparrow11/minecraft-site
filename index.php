42d4e365632df1b84b249a6961350e8715b7d7c2
Cyber Sparrow <79370321+CyberSparrow11@users.noreply.github.com>
2025-03-03 22:56:28 +0300

    Update index.php
    Добавление счетчика Liveinternet

--- index.php
+++ index.php
@@ -7,7 +7,17 @@
 <html>
 
 	<head>
-		
+		<!--LiveInternet counter--><a href="https://www.liveinternet.ru/click"
+target="_blank"><img id="licnt60EA" width="88" height="120" style="border:0" 
+title="LiveInternet: показано количество просмотров и посетителей"
+src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAEALAAAAAABAAEAAAIBTAA7"
+alt=""/></a><script>(function(d,s){d.getElementById("licnt60EA").src=
+"https://counter.yadro.ru/hit?t29.15;r"+escape(d.referrer)+
+((typeof(s)=="undefined")?"":";s"+s.width+"*"+s.height+"*"+
+(s.colorDepth?s.colorDepth:s.pixelDepth))+";u"+escape(d.URL)+
+";h"+escape(d.title.substring(0,150))+";"+Math.random()})
+(document,screen)</script><!--/LiveInternet-->
+
    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 		<title>Сервер</title>
 		<link rel="stylesheet" href="style/shapkastyle.css">
